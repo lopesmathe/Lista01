ecc1a8fa20c0076d35c36db15184ada28b5668d6
lopesmathe <matheuslopesbauer@gmail.com>
2024-03-13 01:25:29 -0300

    ex17

--- ex16.java
+++ ex16.java
@@ -9,8 +9,5 @@ public class ex16 {
         float nota3 = bb.VoltarNumero1F();
         float media = (nota1 + nota2 + nota3)/3;
         System.out.printf("a media das nota 1 %.2f, nota 2 %.2f, nota 3 %.2f a media é %.2f", nota1, nota2, nota3, media);
-         
-
-
     }
 }
--- wbiblioteca.java
+++ wbiblioteca.java
@@ -3,8 +3,10 @@ public class wbiblioteca {
 
     private int num1;
     private int num2;
+    private int num3;
     private float num1F;
     private float num2F;
+    private float num3F;
 
     public void PegarNumeros() {
             Scanner scanner = new Scanner(System.in);
@@ -20,6 +22,26 @@ public class wbiblioteca {
         num1F = scanner.nextFloat();
         System.out.println("Digite o segundo número:");
         num2F = scanner.nextFloat();
+}
+
+    public void PegarNumeross() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Digite o primeiro número:");
+        num1 = scanner.nextInt();
+        System.out.println("Digite o segundo número:");
+        num2 = scanner.nextInt();
+        System.out.println("Digite o terceiro número:");
+        num3 = scanner.nextInt();
+}
+
+    public void PegarNumerossF() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Digite o primeiro número:");
+        num1F = scanner.nextFloat();
+        System.out.println("Digite o segundo número:");
+        num2F = scanner.nextFloat();
+        System.out.println("Digite o terceiro número:");
+        num3F = scanner.nextFloat();
 }
 
     public void PegarNumero() {
@@ -40,12 +62,21 @@ public void PegarNumeroF() {
     public int VoltarNumero2(){
         return num2;
     }
+
+    public int VoltarNumero3(){
+        return num3;
+    }
+
     public float VoltarNumero1F(){
         return num1F;
     }
 
     public float VoltarNumero2F(){
         return num2F;
+    }
+
+    public int VoltarNumero3F(){
+        return num3;
     }
 
     public int ContaDeAdição(int a, int b){
