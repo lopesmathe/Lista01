66cb9d40458dc3197103badfaf5a0e565bfa6270
lopesmathe <matheuslopesbauer@gmail.com>
2024-03-25 09:13:01 -0300

    ex4

---
+++ Ex4.java
@@ -0,0 +1,14 @@
+public class Ex4 {
+    public static void run() {
+        Wbiblioteca biblioteca = new Wbiblioteca();
+        biblioteca.PegarNumeros();
+        int num1 = biblioteca.VoltarNumero1();
+        int num2 = biblioteca.VoltarNumero2();
+        int resu1 = biblioteca.ContaDeAdição(num1, num2);
+        int resu2 = biblioteca.ContaDeSubtração(num1, num2);
+        int resu3 = biblioteca.ContaDeMultiplicação(num1, num2);
+        int resu4 = biblioteca.ContaDeDivisão(num1, num2);
+        System.out.printf("adição = %d  subtrão = %d  multiplicão = %d  divisão = %d", resu1, resu2, resu3, resu4);
+
+    }
+}
