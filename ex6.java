2d1f7ca1cc3581de3cccbbdfc64e45583544b756
lopesmathe <matheuslopesbauer@gmail.com>
2024-03-25 09:07:56 -0300

    mudando

--- ex6.java
+++
@@ -1,9 +0,0 @@
-public class Ex6 {
-    public static void run() {
-        Wbiblioteca biblioteca = new Wbiblioteca();
-        biblioteca.PegarNumeroF();
-        float temperatura = biblioteca.VoltarNumero1F();
-        float Fahrenheit = (9 * temperatura + 160) / 5.0f;
-        System.out.printf("a temperatura é %f", Fahrenheit);
-    }
-}
