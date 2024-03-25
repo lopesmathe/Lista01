d75e961206931291d20f0b7c7c29e4ab57c8ff68
lopesmathe <matheuslopesbauer@gmail.com>
2024-03-25 09:15:18 -0300

    add ex 9

---
+++ Ex9.java
@@ -0,0 +1,18 @@
+public class Ex9 {
+    public static void run() {
+        System.out.println("Me de dois valores e eu irei falar se ele sao iguais ou diferentes");
+        Wbiblioteca biblioteca = new Wbiblioteca();
+        biblioteca.PegarNumeros();
+        int A = biblioteca.VoltarNumero1();
+        int B = biblioteca.VoltarNumero2();
+        if (A == B) {
+            System.out.println("é igual");
+        }
+         else if (A < B){
+            System.out.printf("o umero %d é maior ", B);          
+                System.out.println("");
+            if(A > B)
+            System.out.printf("o umero %d é maior ", A);
+         }
+    }
+}
