02ab45e2134b2dfe6f6ee4b750a9254f04a3245e
lopesmathe <matheuslopesbauer@gmail.com>
2024-03-25 09:15:37 -0300

    add ex 10

---
+++ Ex10.java
@@ -0,0 +1,28 @@
+public class Ex10 {
+    public static void run() {
+        System.out.println("digite o numero de 1 a 5 e ele sera escrito por extenso");
+        Wbiblioteca biblioteca = new Wbiblioteca();
+        biblioteca.PegarNumero();
+        int escolha = biblioteca.VoltarNumero1();
+        switch (escolha) {
+            case 1:
+                System.out.println("Um");
+                break;
+            case 2:
+                System.out.println("Dois");
+                break;
+            case 3:
+                System.out.println("Tres");
+                break;
+            case 4:
+                System.out.println("Quatro");
+                break;
+            case 5:
+                System.out.println("Cinco");
+                break;
+
+            default:
+                break;
+        }
+    }
+}
