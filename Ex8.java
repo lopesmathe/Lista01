135c6e38a02a6c0f93d36ab32e790e07ed71ffe4
lopesmathe <matheuslopesbauer@gmail.com>
2024-03-25 09:14:52 -0300

    add ex8

---
+++ Ex8.java
@@ -0,0 +1,17 @@
+public class Ex8 {
+    public static void run() {
+        System.out.println("Me de um valor e eu irei falar se ele é maior ou menor do q 50");
+        Wbiblioteca biblioteca = new Wbiblioteca();
+        biblioteca.PegarNumero();
+        int num1 = biblioteca.VoltarNumero1();
+        if (num1 > 50) {
+            System.out.println("é maior do q 50");
+
+        }
+        else if(num1 == 50) {
+            System.out.println("é igual a 50");
+        }
+        else
+            System.out.println("o seu é menor do que 50");
+    }
+}
